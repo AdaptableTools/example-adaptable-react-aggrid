05e9ae2c2b80ac33d17d02ec31ce933e6536894e
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2025-03-23 12:35:10 +0200

    replacing config with initial state

--- src/AdaptableAgGrid/AdaptableAgGrid.tsx
+++ src/AdaptableAgGrid/AdaptableAgGrid.tsx
@@ -220,7 +220,7 @@ export const AdaptableAgGrid = () => {
           });
         },
       },
-      predefinedConfig: {
+      initialState: {
         Dashboard: {
           Revision: CONFIG_REVISION,
           Tabs: [
